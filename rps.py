2acf2890a082b2d58773ff97cf678fd50265ddc2
Andreas Michailidis <michandreas001@gmail.com>
2022-04-24 19:08:06 +0300

    win / loss

--- rps.py
+++ rps.py
@@ -1,3 +1,26 @@
 import random
 
-choice = input("Choose your move")
+choices = ["Rock","Paper","Scissors"]
+choice = input("Choose your move \n")
+cpu_choice = random.choice(choices)
+
+print(f"\nYou: {choice}, CPU: {cpu_choice}.\n")
+
+if choice == cpu_choice:
+    print("Tie!")
+elif choice == "Rock":
+    if cpu_choice == "scissors":
+        print("Win!")
+    else:
+        print("Loss!")
+elif choice == "Scissors":
+    if cpu_choice == "paper":
+        print("Win!")
+    else:
+        print("Loss!")
+elif choice == "Paper":
+    if cpu_choice == "rock":
+        print("Win!")
+    else:
+        print("Loss!")
+
