ad0eaf41fae924e0893fdceb5c7f5d900e72403c
Andreas Michailidis <michandreas001@gmail.com>
2022-04-24 19:30:05 +0300

    input checking, score tracking + finishing game

--- rps.py
+++ rps.py
@@ -1,26 +1,59 @@
 import random
 
-choices = ["Rock","Paper","Scissors"]
-choice = input("Choose your move \n")
-cpu_choice = random.choice(choices)
 
-print(f"\nYou: {choice}, CPU: {cpu_choice}.\n")
+score = 0
+keep_going = True
 
-if choice == cpu_choice:
+while keep_going == True:
+ choices = ["Rock","Paper","Scissors"]
+ choice = input("Choose your move, 'Score' to see your current score, or 'Finish' to end game. \n")
+ cpu_choice = random.choice(choices)
+ print(f"\nYou: {choice}, CPU: {cpu_choice}.\n")
+
+ if choice == cpu_choice:
     print("Tie!")
-elif choice == "Rock":
+    score = score
+ elif choice == "Rock":
     if cpu_choice == "scissors":
         print("Win!")
+        score = score + 1
     else:
         print("Loss!")
-elif choice == "Scissors":
-    if cpu_choice == "paper":
+        score = score -1
+ elif choice == "Scissors":
+    if cpu_choice == "Paper":
         print("Win!")
+        score = score + 1
     else:
         print("Loss!")
-elif choice == "Paper":
-    if cpu_choice == "rock":
+        score = score - 1
+ elif choice == "Paper":
+    if cpu_choice == "Rock":
         print("Win!")
+        score = score + 1
     else:
         print("Loss!")
+        score = score - 1
+ elif choice == "Score":
+    if score > 0:
+        print ("You have won " + str(score) + " games")
+    elif score < 0:
+        print ("You have lost " + str(abs(score)) + " games")
+    else:
+        print ("The score is tied")
+ elif choice == "Finish":
+  keep_going =  False
+  if score < 0:
+        print ("Game ended. Your score is positive. You have won " + str(score) + " games")
+  elif score > 0:
+        print ("Game ended. Your score is negative. You have lost " + str(score) + " games")
+  else:
+        print ("The score was tied")
+ else:
+     print ("Invalid input. Please try again.")
+
+
+
+
+
 
